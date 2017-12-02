a896bd188154a713dfd0d674c1c09104c12bbfa3
fennelon <rsbrum@gmail.com>
2017-12-02 04:24:18 -0200

    viewport

--- php/includes/header.inc.php
+++ php/includes/header.inc.php
@@ -7,7 +7,7 @@
 <head>
   <!--   <link rel="stylesheet" href="../todolist/css/style.css">
     <script src="../todolist/js/main.js"></script> -->
-
+    <meta name=viewport content="width=device-width, initial-scale=1, user-scalable=yes">
     <link rel="stylesheet" href="../../css/style.css">
     <script src="../js/main.js"></script>
 </head>
