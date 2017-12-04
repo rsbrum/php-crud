887dabf2d10189e2f1e2037c5392e624c7b534cb
fennelon <rsbrum@gmail.com>
2017-12-04 06:56:20 -0200

    taskbar

--- php/includes/header.inc.php
+++ php/includes/header.inc.php
@@ -5,6 +5,7 @@
 ?>
 <html>
 <head>
+    <meta name="mobile-web-app-capable" content="yes">
     <meta name="apple-mobile-web-app-capable" content="yes" />
     <meta name=viewport content="width=device-width, initial-scale=1, user-scalable=yes">
 
